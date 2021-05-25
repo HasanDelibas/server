579a076dfcbd7ec7243c650a72044fd0a5f917c3
Hasan Delibaş <hasan.delibas94@gmail.com>
2021-05-25 22:47:09 +0300

    Update make.service.sh

--- make.service.sh
+++ make.service.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "File is : "$0"\n"
-cp $1 /etc/systemd/system/
-systemctl enable $1
-systemctl start $1
+if [ -d $1 ];
+then
+    echo "Installing Service $1"
+    cp $1 /etc/systemd/system/
+    systemctl enable $1
+  systemctl start $1
+else
+    echo "Enter a parameter. Parameter must be a .service file"
+fi
+
+
+
 # SOURCE 2 : How To Make Service : https://medium.com/@gokhansengun/linux-servisleri-nasıl-yönetir-7e2a5a8dee00
