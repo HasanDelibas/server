f8260617b0586db8ac3fb5279d308cd0c9eaf804
Hasan Delibaş <hasan.delibas94@gmail.com>
2021-05-25 22:37:19 +0300

    Update make.service.sh

--- make.service.sh
+++ make.service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "File is : "$0"\n"
 cp $1 /etc/systemd/system/
-systemctl enable node.web.service
-systemctl start node.web.service
+systemctl enable $1
+systemctl start $1
 # SOURCE 2 : How To Make Service : https://medium.com/@gokhansengun/linux-servisleri-nasıl-yönetir-7e2a5a8dee00
